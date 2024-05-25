eec6166e3e40dbdc647af45593eea1bcdf30505f
Dmytrotarn <112310528+Dmytrotarn@users.noreply.github.com>
2024-05-25 20:17:46 +0300

    refactored to async

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,16 +2,18 @@
 import axios from 'axios'
 
 const API_KEY = "43985824-437e417885fe4ca625fa28e08"
-
+const perPage = 15
 axios.defaults.baseURL = 'https://pixabay.com/api/'
 
-export async function getSearchResults(search) {
+export async function getSearchResults(search, page) {
     const response = await axios({
         params: {
-            key: "43985824-437e417885fe4ca625fa28e08",
+            key: API_KEY,
             q: search,
             image_type: 'photo',
             safesearch: true,
+            page: page,
+            per_page: perPage,
         }
     })
 
--- src/main.js
+++ src/main.js
@@ -11,8 +11,11 @@ const gallery = document.querySelector('.gallery')
 const load = document.querySelector('.loader')
 const form = document.querySelector('.form')
 const loadMore = document.querySelector('load-more')
+
+let userInput = ''
 let page = 1;
 const perPage = 15;
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
@@ -24,46 +27,46 @@ form.addEventListener('submit', handleSearch)
 async function handleSearch(e) {
     e.preventDefault()
 
-    const searchValue = search.value.trim()
+    userInput = search.value.trim()
+
     // Порожній запит
-    if (searchValue === '') {
+    if (userInput === '') {
         iziToast.error({
             message: 'Input field can not be empty',
             position: "topRight",
             // timeout: false,
             displayMode: 2,
         })
-        search.value = ''
+        form.reset()
         gallery.innerHTML = ''
         return
     }
 
     load.classList.remove('is-hidden')
 
+    try {
+        const { hits, total } = await getSearchResults(userInput, page)
 
-    await getSearchResults(searchValue)
-        .then((data) => {
-            if (data.total === 0) {                             //нічого не знайдено
-                iziToast.show({
-                    message: '"Sorry, there are no images matching your search query. Please try again!"',
-                    color: 'blue',
-                    position: 'topRight',
-                    // timeout: false,
-                    displayMode: 2
-                })
-                load.classList.add('is-hidden')
-                gallery.innerHTML = ''
-                return
-            }
-
-            gallery.innerHTML = renderImages(data.hits)
-            lightbox.refresh()
-        })
-        .catch(e => {
-            iziToast.error({ message: `${e}` })
-        }).finally(() => {
-            search.value = ''
+        if (total === 0) {                             //нічого не знайдено
+            iziToast.show({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                color: 'blue',
+                position: 'topRight',
+                // timeout: false,
+                displayMode: 2
+            })
             load.classList.add('is-hidden')
-        })
-    // iziToast.destroy()
+            gallery.innerHTML = ''
+            form.reset()
+            return
+        }
+        gallery.innerHTML = renderImages(hits)
+        lightbox.refresh()
+    }
+    catch (error) {
+        iziToast.error({ message: `${error}` })
+    }
+    form.reset()
+    load.classList.add('is-hidden')
 }
+    // iziToast.destroy()
